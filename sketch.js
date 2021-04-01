d7fdeb10a80fc784e1995d26ba3bb8c58ec9608a
SlimSpeedster <yashdgandhi@outlook.com>
2021-04-01 18:09:50 -0400

    I have finished the project

--- sketch.js
+++ sketch.js
@@ -1,6 +1,6 @@
 var balloon,balloonImage1,balloonImage2;
 // create database and position variable here
-
+var database, position;
 function preload(){
    bg =loadImage("cityImage.png");
    balloonImage1=loadAnimation("hotairballoon1.png");
@@ -11,13 +11,15 @@ function preload(){
 
 //Function to set initial environment
 function setup() {
-  database=firebase.database();
+ 
   createCanvas(1500,700);
-
+  database = firebase.database();  
   balloon=createSprite(250,450,150,150);
   balloon.addAnimation("hotAirBalloon",balloonImage1);
   balloon.scale=0.5;
-
+//database=firebase.database();
+  position = database.ref('balloon/height');
+  position.on('value', readPosition, showError);
   textSize(20); 
 }
 
@@ -27,19 +29,19 @@ function draw() {
 
   if(keyDown(LEFT_ARROW)){
     balloon.addAnimation("hotAirBalloon",balloonImage2);
-    //write code to move air balloon in left direction
+    changePosition(-2,0);
   }
   else if(keyDown(RIGHT_ARROW)){
     balloon.addAnimation("hotAirBalloon",balloonImage2);
-    //write code to move air balloon in right direction
+    changePosition(2,0);
   }
   else if(keyDown(UP_ARROW)){
     balloon.addAnimation("hotAirBalloon",balloonImage2);
-    //write code to move air balloon in up direction
+    changePosition(0,-2);
   }
   else if(keyDown(DOWN_ARROW)){
     balloon.addAnimation("hotAirBalloon",balloonImage2);
-    //write code to move air balloon in down direction
+    changePosition(0,2);
   }
 
   drawSprites();
@@ -47,4 +49,29 @@ function draw() {
   stroke("white");
   textSize(25);
   text("**Use arrow keys to move Hot Air Balloon!",40,40);
+}
+ 
+function changePosition(x,y){
+    database.ref('balloon/height').set({
+        'x': position.x+x,
+        'y': position.y+y
+
+    }
+)
+
+
+
+}
+
+function readPosition(data){
+    position = data.val();
+    balloon.x = position.x;
+     balloon.y = position.y;
+
+}
+
+function showError(){
+
+console.log("there is an error! go fix it.")
+
 }
